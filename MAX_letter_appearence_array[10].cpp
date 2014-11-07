84a6f0a74592549fb56fcf2cb1b841f927d38938
Kumo <destroyer_raigeki@hotmail.com>
2014-11-07 13:21:36 +0200

    Create MAX_letter_appearence_array[10].cpp

---
+++ MAX_letter_appearence_array[10].cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+system("color f1");
+
+		string a[10];
+		string word;
+		int count[256];
+		int i,j,k,max;
+		char c;
+		
+		for (i=0; i<10; ++i)
+		{
+			cout<<"Dwse le3h "<<i+1<<": ";
+			getline(cin,a[i]);
+		}
+				
+		for (k=0; k<256; k++)
+		{
+			count[k]=0;
+		}
+		
+		for (i=0; i<10; i++)
+		{
+			word=a[i];
+			for (j=0; j<word.size(); j++)
+			{	
+				for (k=0; k<256; k++)
+				{
+					if (word[j]==k)
+					{
+						++count[k];
+					}
+				}
+			}
+		}
+		
+		max=0;
+		for (k=1; k<256; k++)
+		{
+			if (count[k]>max)
+			{
+				max=count[k];
+				c=k;
+			}
+		}
+		
+		cout<<"Poio polles fores emfanizetai to '"<<c<<"' "<<max<<" fores"<<endl;
+		
+system("pause");
+}
